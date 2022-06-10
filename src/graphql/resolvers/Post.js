4d8befaabe80c071e368272eb4475683aea3f4f7
yzeren <yilmazzeren93@gmail.com>
2022-06-10 16:15:37 +0300

    Fix

--- src/graphql/resolvers/Post.js
+++
@@ -1,9 +0,0 @@
-const Post = {
-  user: (parent, _, { db }) =>
-    db.users.find((user) => user.id === parent.user_id),
-  comments: (parent, _, { db }) => {
-    return db.comments.filter((comment) => comment.post_id === parent.id);
-  },
-};
-
-export default Post;
--- src/graphql/resolvers/User.js
+++
@@ -1,9 +0,0 @@
-const User = {
-  posts: (parent, _, { db }) =>
-    db.posts.filter((post) => post.user_id === parent.id),
-  comments: (parent, _, { db }) => {
-    return db.comments.filter((comment) => comment.user_id === parent.id);
-  },
-};
-
-export default User;
